7cef5c6e8fb3910d9fcdee355656e044832a9b66
skushmak11 <skushmak@gmail.com>
2025-07-27 16:42:58 +0530

    Add vite.config.ts 1753614777366

---
+++ vite.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 8000,
+    host: true,
+    allowedHosts: ['6sqm23-8000.csb.app']
+  }
+})
